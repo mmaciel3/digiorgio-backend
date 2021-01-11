81ae573417131555ffcdd1b7c6730b7cb9cd0be2
Matheus Soares <matheus.soares@adp.com>
2021-01-10 21:44:11 -0300

    Create API that stores the user agent in a S3 bucket as a very basic form of analytics system

--- analytics.js
+++ analytics.js
@@ -1,6 +1,26 @@
-module.exports.save = async event => {
-	console.log(`Saving analytics event... ${JSON.stringify(event)}`);
-	return {
-		statusCode: 200
+const objectPath = require('simple-object-path');
+const corsResponse = require('./lib/corsResponse');
+const s3 = require('./lib/s3');
+
+const { ANALYTICS_BUCKET_NAME, ANALYTICS_FILE_NAME_PREFIX } = process.env;
+
+const createFileContent = event => {
+	const content = {
+		userAgent: objectPath(event, 'headers/User-Agent')
 	};
+	return JSON.stringify(content, null, 4);
+};
+
+module.exports.save = async event => {
+	try {
+		const userAgent = createFileContent(event);
+		const fileName = `${ANALYTICS_FILE_NAME_PREFIX}${Date.now()}.json`;
+		await s3.createFile(ANALYTICS_BUCKET_NAME, fileName, userAgent);
+	} catch (err) {
+		console.error(`Error when storing analytics: ${err.stack}`);
+	}
+
+	return corsResponse({
+		statusCode: 200
+	});
 };
