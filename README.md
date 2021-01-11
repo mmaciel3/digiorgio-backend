d6125227516fc3efcbb07ef151a71464d1ae2036
Matheus Soares <matheus.soares@adp.com>
2021-01-11 15:19:17 -0300

    Using email templates instead of hard coding the email text

--- README.md
+++ README.md
@@ -4,3 +4,24 @@
 2. Grant execution permission to `deploy.sh`: `chmod +x deploy.sh`.
 3. Update the content of `deploy.sh` with your AWS key and secret.
 4. Run `./deploy.sh`.
+
+# Email templates
+
+AWS does not provide a way to do it through the console so far. The CLI is the only way to do it.
+The latest version of email templates should be kept at the `templates` folder so that it's easier to modify them.
+
+## Create a new template
+
+`aws ses create-template --cli-input-json file://templates/BrandLead.json --region sa-east-1`
+
+## List existing templates
+
+`aws ses list-templates --region sa-east-1`
+
+## Get template content
+
+`aws ses get-template --template-name BrandLead --region sa-east-1`
+
+## Update existing template
+
+`aws ses update-template --cli-input-json file://templates/BrandLead.json --region sa-east-1`
--- leads.js
+++ leads.js
@@ -1,28 +1,23 @@
 const corsResponse = require('./lib/corsResponse');
 const emailService = require('./lib/emailService');
 
-const { LEAD_EMAIL_RECIPIENT, LEAD_EMAIL_SENDER, LEAD_EMAIL_SUBJECT } = process.env;
-
-const createBody = event => {
-	const body = JSON.parse(event.body) || {};
-	const { brand, type, description } = body;
-	return `Marca: ${brand}\nTipo: ${type}\nDescrição:${description}`;
-};
+const { LEAD_EMAIL_RECIPIENT, LEAD_EMAIL_SENDER, LEAD_EMAIL_TEMPLATE_NAME } = process.env;
 
 module.exports.notify = async event => {
+	let statusCode = 200;
+
 	try {
-		const body = createBody(event);
-		await emailService.sendEmail({
+		const variables = JSON.parse(event.body) || {};
+		await emailService.sendTemplatedEmail({
 			from: LEAD_EMAIL_SENDER,
 			to: LEAD_EMAIL_RECIPIENT,
-			subject: LEAD_EMAIL_SUBJECT,
-			body
+			template: LEAD_EMAIL_TEMPLATE_NAME,
+			variables
 		});
 	} catch (err) {
+		statusCode = 500;
 		console.error(`Error when notifying lead: ${err.stack}`);
 	}
 
-	return corsResponse({
-		statusCode: 200
-	});
+	return corsResponse({ statusCode });
 };
